f123a9d5ba807ff75bfc5a6639865bd37296f5fd
Rüveyda Nur Güneş <rgunees07@gmail.com>
2026-02-01 19:53:25 +0300

    dosya duzenlemeleri

---
+++ experiments/fallenpersondataset/notebooks and codes/train_v11.py
@@ -0,0 +1,50 @@
+from ultralytics import YOLO
+import os
+
+# ===============================
+# DATASET YOLU
+# ===============================
+DATA_YAML = "dataset2/data.yaml"
+
+if __name__ == '__main__':
+    if not os.path.exists(DATA_YAML):
+        raise FileNotFoundError(f"{DATA_YAML} bulunamadı")
+
+    # ===============================
+    # MODEL 
+    # ===============================
+    model = YOLO("yolo11n.pt")  
+    # ===============================
+    # TRAIN
+    # ===============================
+    model.train(
+        data=DATA_YAML,
+        epochs=100,
+        imgsz=640,
+        batch=16,      
+        device=0,        
+        workers=8,       
+        cache=True,     
+        amp=True,       
+
+        # ===== AUGMENTATION KAPALI =====
+        augment=False,
+        mosaic=0.0,
+        mixup=0.0,
+        copy_paste=0.0,
+        hsv_h=0.0,
+        hsv_s=0.0,
+        hsv_v=0.0,
+        degrees=0.0,
+        translate=0.0,
+        scale=0.0,
+        shear=0.0,
+        perspective=0.0,
+        flipud=0.0,
+        fliplr=0.0,
+
+        seed=42,
+        deterministic=True,    
+    )
+
+    print("✅ Eğitim tamamlandı")
